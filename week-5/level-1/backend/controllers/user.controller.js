022f1a6c82ba23d5511fe8e9f8e24c309e7c2595
rajmanna <raj2021manna@gmail.com>
2025-03-12 15:40:05 +0530

    enhancement: add separate controllers

---
+++ week-5/level-1/backend/controllers/user.controller.js
@@ -0,0 +1,76 @@
+import { User } from '../db/index.js';
+import { userSchema } from '../schema/types.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+config();
+
+export const signUpUser = async (req, res) => {
+  const createPayload = req.body;
+  const parsePayload = userSchema.safeParse(createPayload);
+
+  if (!parsePayload.success) {
+    return res.status(411).json({
+      message: 'Signup failed.',
+    });
+  }
+
+  const isExist = await User.findOne({ username: createPayload.username });
+  if (isExist) {
+    return res.status(409).json({
+      message: 'Username already exist. Please try to signin.',
+    });
+  }
+
+  const passwordSalt = await bcrypt.genSalt(parseInt(process.env.SALT));
+  const passwordHash = await bcrypt.hash(createPayload.password, passwordSalt);
+
+  await User.create({
+    username: createPayload.username,
+    password: passwordHash,
+  });
+
+  res.status(201).json({
+    message: 'User created successfully. Proceed to signin',
+  });
+};
+
+export const signInUser = async (req, res) => {
+  const createPayload = req.body;
+  const parsePayload = userSchema.safeParse(createPayload);
+
+  if (!parsePayload.success) {
+    return res.status(411).json({
+      message: 'Signup failed.',
+    });
+  }
+
+  const user = await User.findOne({ username: createPayload.username });
+  if (!user) {
+    return res.status(411).json({
+      message: "User doesn't exist. Please signin first.",
+    });
+  }
+
+  const verifyPassword = await bcrypt.compare(
+    createPayload.password,
+    user.password
+  );
+
+  if (!verifyPassword) {
+    return res.status(411).json({
+      message: 'Incorrect credentials.',
+    });
+  }
+
+  const token = jwt.sign(
+    { username: createPayload.username },
+    process.env.JWT_SECRET
+  );
+
+  res.status(200).json({
+    token: token,
+    message: 'User authenticated successfully.',
+  });
+};
--- week-5/level-1/backend/routers/card.router.js
+++ week-5/level-1/backend/routers/card.router.js
@@ -1,111 +1,16 @@
 import { Router } from 'express';
-import { Card, User } from '../db/index.js';
-import { cardSchema } from '../schema/types.js';
-import { parse } from 'dotenv';
+import {
+  createCard,
+  deleteCard,
+  getCards,
+  updateCard,
+} from '../controllers/card.controller.js';
 
 const router = Router();
 
-// Get all cards
-router.get('/dashboard', async (req, res) => {
-  const user = await User.findOne({ username: req.username });
-  const cards = await Card.find({
-    _id: {
-      $in: user.createdCards,
-    },
-  });
-
-  res.status(200).json({
-    cards: cards,
-  });
-});
-
-// Create new card
-router.post('/create', async (req, res) => {
-  const createPayload = req.body;
-  const parsePayload = cardSchema.safeParse(createPayload);
-
-  if (!parsePayload.success) {
-    return res.status(411).json({
-      message: 'Unable to create card.',
-    });
-  }
-
-  const newCard = await Card.create({
-    firstName: createPayload.firstName,
-    lastName: createPayload.lastName,
-    bio: createPayload.bio,
-    interests: createPayload.interests,
-    twitter: createPayload.twitter,
-    instagram: createPayload.instagram,
-    facebook: createPayload.facebook,
-    youtube: createPayload.youtube,
-    linkedIn: createPayload.linkedIn,
-  });
-
-  await User.updateOne(
-    { username: req.username },
-    { $push: { createdCards: newCard._id } }
-  );
-
-  res.status(201).json({
-    message: 'Card created successfully.',
-  });
-});
-
-// Update existing card
-router.put('/update/:id', async (req, res) => {
-  const id = req.params.id;
-  const isExist = await Card.findById(id);
-  if (!isExist) {
-    return res.status(404).json({
-      message: "Card doesn't exist.",
-    });
-  }
-  const createPayload = req.body;
-  const parsePayload = cardSchema.safeParse(createPayload);
-  if (!parsePayload.success) {
-    return res.status(411).json({
-      message: 'Unable to update card.',
-    });
-  }
-
-  await Card.findByIdAndUpdate(
-    { _id: id },
-    {
-      firstName: createPayload.firstName,
-      lastName: createPayload.lastName,
-      bio: createPayload.bio,
-      interests: createPayload.interests,
-      twitter: createPayload.twitter,
-      instagram: createPayload.instagram,
-      facebook: createPayload.facebook,
-      youtube: createPayload.youtube,
-      linkedIn: createPayload.linkedIn,
-    },
-    { new: true }
-  );
-
-  res.status(200).json({
-    message: 'Updating card successfully',
-  });
-});
-
-// Delete existing card
-router.delete('/delete/:id', async (req, res) => {
-  const id = req.params.id;
-  await Card.findByIdAndDelete(id);
-
-  await User.findOneAndUpdate(
-    { username: req.username },
-    {
-      $pull: { createdCards: id },
-    },
-    { new: true }
-  );
-
-  res.status(200).json({
-    message: 'Deleting card successfully.',
-  });
-});
+router.get('/dashboard', getCards);
+router.post('/create', createCard);
+router.put('/update/:id', updateCard);
+router.delete('/delete/:id', deleteCard);
 
 export default router;
