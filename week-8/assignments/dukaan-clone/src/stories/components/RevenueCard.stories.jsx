59123965248c9d63d8f718f116f18483557ff2ca
rajmanna <raj2021manna@gmail.com>
2025-03-29 11:04:32 +0530

    warp up 8.1

---
+++ week-8/assignments/dukaan-clone/src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,21 @@
+import RevenueCard from "../../components/RevenueCard";
+
+export default {
+  component: RevenueCard,
+};
+
+export const BigAmount = {
+  args: {
+    title: "Asd",
+    orderCount: "123",
+    amount: "10000000",
+  },
+};
+
+export const SmallAmount = {
+  args: {
+    title: "Asd",
+    orderCount: "123",
+    amount: "100",
+  },
+};
