f5eb4f89a847142e8474a7a091a946fe2ae46220
rajmanna <raj2021manna@gmail.com>
2025-04-01 12:33:59 +0530

    ts basic 9.1

---
+++ week-9/my-notes/node-app/src/a.ts
@@ -0,0 +1,55 @@
+interface Person {
+  name: string;
+  age: number;
+  greet(phase: string): void;
+}
+
+// Implements & extends class from interfaces
+class Employee implements Person {
+  name: string;
+  age: number;
+
+  constructor(n: string, a: number) {
+    this.name = n;
+    this.age = a;
+  }
+
+  greet(phase: string) {
+    console.log(`${phase} ${this.name}`);
+  }
+}
+
+const e = new Employee('Raj', 21);
+console.log(e.name);
+
+//-------------------------------------------
+
+// Types
+type Workers = {
+  name: string;
+  startingData: string;
+};
+
+type Manager = {
+  name: string;
+  department: string;
+};
+
+// Union
+type userId = string | number;
+
+// Intersection
+type TechLead = Workers & Manager;
+
+//----------------------------------------------
+
+// Array
+function maxVal(arr: number[]) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+
+maxVal([1, 2, 4]);
